5a6f5f9b1898cb088543cfc4a4999c7a3d1fe373
Gonçalo Costa <goncalo.costa@bravantic.com>
2026-09-15 16:44:43 +0100

    Commit inicial + Saudacao

---
+++ Exercicios_MVC/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Exercicios_MVC.Models;
+
+namespace Exercicios_MVC.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult Saudacao()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None,
+        NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel
+        {
+            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+        });
+    }
+}
